663ae17531cdeb8765af7d8792b5f3ee42ad73e5
Fabio Spampinato <spampinabio@gmail.com>
2024-01-30 22:43:04 +0000

    Ensuring calling the function with .call, .apply and .bind works too

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ describe ( 'Siero Worker', it => {
 
   it ( 'can pass values back and forth between realms', async t => {
 
-    t.plan ( 2 );
+    t.plan ( 8 );
 
     const worker = new SieroWorker ();
 
@@ -27,8 +27,14 @@ describe ( 'Siero Worker', it => {
     };
 
     const plugin = async () => {
-      const result = await API.sumWith ( 4, 9, Math.sqrt );
-      await API.is ( result, 5 );
+      const result1 = await API.sumWith ( 4, 9, Math.sqrt );
+      await API.is ( result1, 5 );
+      const result2 = await API.sumWith.call ( null, 4, 9, Math.sqrt );
+      await API.is ( result2, 5 );
+      const result3 = await API.sumWith.apply ( null, [4, 9, Math.sqrt] );
+      await API.is ( result3, 5 );
+      const result4 = await API.sumWith.bind ( null, 4, 9, Math.sqrt )();
+      await API.is ( result4, 5 );
     };
 
     worker.global ( 'API', API );
